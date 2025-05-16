c3d5ed4bcaa5d811b266fab1b2000401f4071d08
proxyerium <proxyerium@proton.me>
2025-05-16 11:25:12 +0800

    update: `index.ts` -> `index.js`

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/translate',
+      name: 'translate',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/TabView.vue'),
+    },
+    {
+      path: '/timing',
+      name: 'timing',
+      component: () => import('../views/TabView.vue'),
+    },
+    {
+      path: '/effect',
+      name: 'effect',
+      component: () => import('../views/TabView.vue'),
+    },
+    {
+      path: '/verify',
+      name: 'verify',
+      component: () => import('../views/TabView.vue'),
+    },
+  ],
+})
+
+export default router
